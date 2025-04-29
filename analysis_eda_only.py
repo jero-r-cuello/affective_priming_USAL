b5adbc524bc7599458ee996cb49cea7c4916960d
jero-r-cuello <jerorodriguezcuello231@gmail.com>
2025-04-29 12:32:04 -0300

    Updates in analysis_eda_only, cvxEDA from pyEDA added and tests with it

--- analysis_eda_only.py
+++ analysis_eda_only.py
@@ -7,8 +7,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 import mne
 import neurokit2 as nk
+import cvxopt as cv
 
-#%% Configuración de MNE
+#%% Configuración de los canales
 info_canales = pd.read_excel("Informacion canales eeg (mapa canales).xlsx")
 
 ch_names = list(info_canales["labels"])
@@ -22,11 +23,116 @@ ch_types[20] = 'stim'
 ch_names[20] = 'estimulo'
 ch_types[19] = 'ecg'
 
+#%% Definición de funciones
+
+# Función cvxEDA de pyEDA, no la pude importar
+def cvxEDA_pyEDA(y, delta, tau0=2., tau1=0.7, delta_knot=10., alpha=8e-4, gamma=1e-2,
+           solver=None, options={'reltol':1e-9}):
+    """CVXEDA Convex optimization approach to electrodermal activity processing
+    This function implements the cvxEDA algorithm described in "cvxEDA: a
+    Convex Optimization Approach to Electrodermal Activity Processing"
+    (http://dx.doi.org/10.1109/TBME.2015.2474131, also available from the
+    authors' homepages).
+    Arguments:
+       y: observed EDA signal (we recommend normalizing it: y = zscore(y))
+       delta: sampling interval (in seconds) of y
+       tau0: slow time constant of the Bateman function
+       tau1: fast time constant of the Bateman function
+       delta_knot: time between knots of the tonic spline function
+       alpha: penalization for the sparse SMNA driver
+       gamma: penalization for the tonic spline coefficients
+       solver: sparse QP solver to be used, see cvxopt.solvers.qp
+       options: solver options, see:
+                http://cvxopt.org/userguide/coneprog.html#algorithm-parameters
+    Returns (see paper for details):
+       r: phasic component
+       p: sparse SMNA driver of phasic component
+       t: tonic component
+       l: coefficients of tonic spline
+       d: offset and slope of the linear drift term
+       e: model residuals
+       obj: value of objective function being minimized (eq 15 of paper)
+    """
+    
+    n = len(y)
+    y = cv.matrix(y)
+
+    # bateman ARMA model
+    a1 = 1./min(tau1, tau0) # a1 > a0
+    a0 = 1./max(tau1, tau0)
+    ar = np.array([(a1*delta + 2.) * (a0*delta + 2.), 2.*a1*a0*delta**2 - 8.,
+        (a1*delta - 2.) * (a0*delta - 2.)]) / ((a1 - a0) * delta**2)
+    ma = np.array([1., 2., 1.])
+
+    # matrices for ARMA model
+    i = np.arange(2, n)
+    A = cv.spmatrix(np.tile(ar, (n-2,1)), np.c_[i,i,i], np.c_[i,i-1,i-2], (n,n))
+    M = cv.spmatrix(np.tile(ma, (n-2,1)), np.c_[i,i,i], np.c_[i,i-1,i-2], (n,n))
+
+    # spline
+    delta_knot_s = int(round(delta_knot / delta))
+    spl = np.r_[np.arange(1.,delta_knot_s), np.arange(delta_knot_s, 0., -1.)] # order 1
+    spl = np.convolve(spl, spl, 'full')
+    spl /= max(spl)
+    # matrix of spline regressors
+    i = np.c_[np.arange(-(len(spl)//2), (len(spl)+1)//2)] + np.r_[np.arange(0, n, delta_knot_s)]
+    nB = i.shape[1]
+    j = np.tile(np.arange(nB), (len(spl),1))
+    p = np.tile(spl, (nB,1)).T
+    valid = (i >= 0) & (i < n)
+    B = cv.spmatrix(p[valid], i[valid], j[valid])
+
+    # trend
+    C = cv.matrix(np.c_[np.ones(n), np.arange(1., n+1.)/n])
+    nC = C.size[1]
+
+    # Solve the problem:
+    # .5*(M*q + B*l + C*d - y)^2 + alpha*sum(A,1)*p + .5*gamma*l'*l
+    # s.t. A*q >= 0
+
+    old_options = cv.solvers.options.copy()
+    cv.solvers.options.clear()
+    cv.solvers.options.update(options)
+    if solver == 'conelp':
+        # Use conelp
+        z = lambda m,n: cv.spmatrix([],[],[],(m,n))
+        G = cv.sparse([[-A,z(2,n),M,z(nB+2,n)],[z(n+2,nC),C,z(nB+2,nC)],
+                    [z(n,1),-1,1,z(n+nB+2,1)],[z(2*n+2,1),-1,1,z(nB,1)],
+                    [z(n+2,nB),B,z(2,nB),cv.spmatrix(1.0, range(nB), range(nB))]])
+        h = cv.matrix([z(n,1),.5,.5,y,.5,.5,z(nB,1)])
+        c = cv.matrix([(cv.matrix(alpha, (1,n)) * A).T,z(nC,1),1,gamma,z(nB,1)])
+        res = cv.solvers.conelp(c, G, h, dims={'l':n,'q':[n+2,nB+2],'s':[]})
+        obj = res['primal objective']
+    else:
+        # Use qp
+        Mt, Ct, Bt = M.T, C.T, B.T
+        H = cv.sparse([[Mt*M, Ct*M, Bt*M], [Mt*C, Ct*C, Bt*C], 
+                    [Mt*B, Ct*B, Bt*B+gamma*cv.spmatrix(1.0, range(nB), range(nB))]])
+        f = cv.matrix([(cv.matrix(alpha, (1,n)) * A).T - Mt*y,  -(Ct*y), -(Bt*y)])
+        res = cv.solvers.qp(H, f, cv.spmatrix(-A.V, A.I, A.J, (n,len(f))),
+                            cv.matrix(0., (n,1)), solver=solver)
+        obj = res['primal objective'] + .5 * (y.T * y)
+    cv.solvers.options.clear()
+    cv.solvers.options.update(old_options)
+
+    l = res['x'][-nB:]
+    d = res['x'][n:n+nC]
+    t = B*l + C*d
+    q = res['x'][:n]
+    p = A * q
+    r = M * q
+    e = y - r - t
+
+    return (np.array(a).ravel() for a in (r, p, t, l, d, e, obj))
+
 #%% Leer los datos 
 carpeta = "datos_physio"
-subjects = [archivo for archivo in os.listdir(carpeta) if not archivo.endswith(".csv")]
+subjects = [archivo for archivo in os.listdir(carpeta) if not archivo.endswith(".csv") and not archivo.endswith(".TXT")]
 
-excluded_subjects = {"S4": "El canal de estímulo está roto"}
+# Sujetos excluídos junto con el motivo
+excluded_subjects = {"S4": "El canal de estímulo está roto",
+                     "S2": "Se sacó el scr a mitad del exp 1",
+                     "S1": "No hay datos, creo que este solo fue una prueba"}
 
 list_dfs = []
 
@@ -63,59 +169,48 @@ for i, (df_physio, df_beh) in enumerate(list_dfs):
     plt.legend()
     plt.show()
 
-#%% Estaría bueno primero separarlo por exp y después hacer esto
+#%% EDA - Extracción de features
 
 list_dfs_eda = []
+list_coefs_ajuste = []
+
 for i, (df_physio, df_beh) in enumerate(list_dfs):
-    eda_clean = nk.eda_clean(df_physio["eda"], sampling_rate=512, method="BioSPPy")
-    df_eda = nk.eda_phasic(eda_clean, sampling_rate=512, method="highpass")
-    df_eda["EDA_Clean"] = eda_clean
-    df_eda["EDA_Raw"] = df_physio["eda"]
-    df_eda["subject"] = df_beh["subject"][0]
-    
-    plt.figure()
-    plt.plot(df_eda["EDA_Raw"], label="EDA Raw")
-    plt.plot(df_eda["EDA_Clean"], label="EDA Clean")
-    plt.legend()
-    plt.title(df_beh["subject"][0])
-    plt.show()
-    
+    # Dividir el dataframe según df_beh.exp
+    for j, exp in enumerate(df_beh["exp"].unique()):
+        df_eda = pd.DataFrame()
+        
+        if j == 0:
+            onset_exp = df_beh[df_beh["exp"] == exp].onset.max()
+            df_exp = df_physio.iloc[:int(onset_exp)+500]
 
-    list_dfs_eda.append(df_eda)
-#%%
+        else:
+            onset_exp = df_beh[df_beh["exp"] == exp].onset.min()
+            df_exp = df_physio.iloc[int(onset_exp)-500:]
+        
+        # Extraemos features de EDA
+        eda_clean = nk.eda_clean(df_exp["eda"], sampling_rate=256, method="BioSPPy")
+        df_eda["EDA_Phasic"], df_eda["SMNA"], df_eda["EDA_Tonic"],_,_, df_eda["error"], coef_ajuste = cvxEDA_pyEDA(eda_clean, delta=1/256)
+        df_eda["EDA_Clean"] = eda_clean
 
+        # Está habiendo un tema de que los raws siempre que j = 1 es NaN
+        df_eda["EDA_Raw"] = df_exp["eda"]
+        df_eda["subject"] = df_beh["subject"][0]
+        df_eda["exp"] = exp
+        
+        plt.figure()
+        plt.plot(df_eda["EDA_Raw"], label="EDA Raw")
+        plt.plot(df_eda["EDA_Clean"], label="EDA Clean")
+        plt.legend()
+        plt.title(f'{df_beh["subject"][0]} - {exp}')
+        plt.show()
+        
+        print(f'Coef. de ajuste {df_beh["subject"][0]} - {exp}: {coef_ajuste}')
 
-# %% Probando eventos a partir de marcadores
+        list_coefs_ajuste.append(coef_ajuste)
+        list_dfs_eda.append(df_eda)
 
+        df_eda.to_csv(f"datos_physio/{df_beh['subject'][0]}/df_eda_{df_beh['subject'][0]}_{exp}.csv", index=False)
 
+    pd.concat(list_dfs_eda).to_csv(f"datos_physio/eda_all_subjects_full_exps.csv", index=False)
 
-# Ploteo entero
-plt.figure()
-plt.plot(canal_stim, label="Estimulo")
-for x in events_dict["onset"]:
-    plt.axvline(x=x, color='red', linestyle='--', linewidth=2)
-plt.show()
-
-#%% Crear las anotaciones
-onset_times = events_dict["onset"]/raw.info["sfreq"]
-
-annotations = mne.Annotations(onset=onset_times, 
-                              duration=[0] * len(events_dict["onset"]),
-                              description=["estímulo"] * len(events_dict["onset"]))
-
-raw.set_annotations(annotations)
-
-raw.plot(scalings=scalings)
-
-# Crear un nuevo objeto de anotaciones excluyendo las primeras 12
-new_annotations = mne.Annotations(
-    onset=annotations.onset[12:],  # Omitir los primeros 12 onsets
-    duration=annotations.duration[12:],  # Omitir las primeras 12 duraciones
-    description=annotations.description[12:]  # Omitir las primeras 12 descripciones
-)
-
-# Asignar las nuevas anotaciones al raw
-raw.set_annotations(new_annotations)
-
-raw.plot(scalings=scalings)
-
+# %%
